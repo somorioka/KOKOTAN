5106a2621a30f1f9985a20cea6dbe23f44520362
morioka <sou.kokomirai@gmail.com>
2024-06-07 21:32:02 +0900

    リモートからExcelファイルをダウンロード

--- lib/pages/flashcard_list_screen.dart
+++ lib/pages/flashcard_list_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+import 'package:excel/excel.dart';
 import 'package:flutter/material.dart';
 import 'package:kokotan/db/database_helper.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
 
 class FlashcardListScreen extends StatefulWidget {
   @override
@@ -8,9 +12,11 @@ class FlashcardListScreen extends StatefulWidget {
 
 class _FlashcardListScreenState extends State<FlashcardListScreen> {
   List<Map<String, dynamic>> _cards = [];
-  bool _isLoading = true;
-  TextEditingController _searchController = TextEditingController();
+  bool _isLoading = false;
+  final TextEditingController _searchController = TextEditingController();
   List<Map<String, dynamic>> _searchResults = [];
+  double _downloadProgress = 0.0;
+  bool _dataFetched = false; // データが最初にフェッチされたかどうかを示すフラグ
 
   @override
   void initState() {
@@ -24,9 +30,82 @@ class _FlashcardListScreenState extends State<FlashcardListScreen> {
     final cards = await dbHelper.queryAllRows();
     setState(() {
       _cards = cards;
-      print('Fetched ${_cards.length} cards');
+      _searchResults = cards;
+      _dataFetched = cards.isNotEmpty;
       _isLoading = false;
     });
+  }
+
+  Future<void> _downloadAndImportExcel() async {
+    setState(() {
+      _isLoading = true;
+      _downloadProgress = 0.0;
+    });
+
+    const url =
+        'https://kokomirai.jp/wp-content/uploads/2024/06/dev_kokotan_list.xlsx'; // インターネット上のExcelファイルのURL
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final bytes = response.bodyBytes;
+      final directory = await getApplicationDocumentsDirectory();
+      final file = File('${directory.path}/dev_kokotan_list.xlsx');
+      await file.writeAsBytes(bytes);
+
+      await _importExcelToDatabase(file);
+
+      setState(() {
+        _isLoading = false;
+      });
+
+      _fetchCards(); // Fetch the newly imported cards from the database
+      print("Excel downloaded and imported successfully！");
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+      print('Error downloading file: ${response.statusCode}');
+    }
+  }
+
+  Future<void> _importExcelToDatabase(File file) async {
+    final dbHelper = DatabaseHelper.instance;
+
+    try {
+      var bytes = file.readAsBytesSync();
+      var excel = Excel.decodeBytes(bytes);
+
+      for (var table in excel.tables.keys) {
+        var sheet = excel.tables[table];
+        for (var row in sheet!.rows) {
+          if (row[0] == 'id') continue; // Skip the header row
+
+          Map<String, dynamic> card = {
+            'id': row.length > 0
+                ? int.tryParse(row[0]?.value.toString() ?? '')
+                : null,
+            'word': row.length > 1 ? row[1]?.value.toString() : '',
+            'main_meaning': row.length > 2 ? row[2]?.value.toString() : '',
+            'sub_meaning': row.length > 3 ? row[3]?.value.toString() : '',
+            'sentence': row.length > 4 ? row[4]?.value.toString() : '',
+            'sentence_jp': row.length > 5 ? row[5]?.value.toString() : ''
+          };
+
+          if (card['id'] == null ||
+              card['word'] == '' ||
+              card['main_meaning'] == '' ||
+              card['sentence'] == '' ||
+              card['sentence_jp'] == '') {
+            continue;
+          }
+
+          await dbHelper.insert(card);
+          print('Inserted card: $card'); // デバッグメッセージ追加
+        }
+      }
+      print('Excel data imported successfully');
+    } catch (e) {
+      print('Error importing Excel data: $e');
+    }
   }
 
   void _search(String query) {
@@ -46,27 +125,43 @@ class _FlashcardListScreenState extends State<FlashcardListScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flashcards'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: _downloadAndImportExcel,
+          ),
+        ],
       ),
       body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  LinearProgressIndicator(value: _downloadProgress),
+                  const SizedBox(height: 20),
+                  const Text('Downloading...'),
+                ],
+              ),
+            )
           : Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    controller: _searchController,
-                    decoration: InputDecoration(
-                      labelText: 'Search',
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.search),
-                        onPressed: () {
-                          _search(_searchController.text);
-                        },
+                if (_dataFetched)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: InputDecoration(
+                        labelText: 'Search',
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.search),
+                          onPressed: () {
+                            _search(_searchController.text);
+                          },
+                        ),
                       ),
+                      onChanged: _search,
                     ),
-                    onChanged: _search,
                   ),
-                ),
                 Expanded(
                   child: ListView.builder(
                     itemCount: _searchResults.length,
