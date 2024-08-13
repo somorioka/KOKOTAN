755d53f0765a050dba734c9ca4892d194c50a0ec
morioka <sou.kokomirai@gmail.com>
2024-08-13 16:34:16 +0900

    ホーム画面のUIを修正

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -8,69 +8,81 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final items = [
       {
-        'title': 'ベーシック',
+        'title': 'ベーシック (準備中)',
         'subtitle': 'ターゲット1400, システム英単語basicレベル',
-        'icon': Icons.cloud_download,
+        'icon': Icons.chevron_right,
+        'color': const Color.fromARGB(255, 236, 236, 236),
       },
       {
         'title': 'スタンダードA',
         'subtitle': 'ターゲット1900, システム英単語 前半レベル',
-        'icon': Icons.cloud_download,
+        'icon': Icons.chevron_right,
+        'color': Colors.white,
       },
       {
-        'title': 'スタンダードB',
+        'title': 'スタンダードB (準備中)',
         'subtitle': 'ターゲット1900, システム英単語 後半レベル',
-        'icon': Icons.cloud_download,
+        'icon': Icons.chevron_right,
+        'color': const Color.fromARGB(255, 236, 236, 236),
       },
     ];
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('ココタン'),
+        title: const Text('ホーム'),
       ),
-      body: Column(
-        children: [
-          Container(
-              height: 241,
-              width: 361,
-              child: Image.asset('assets/images/home_humor1.png')),
-          Expanded(
-            child: Consumer<DataViewModel>(
-              builder: (context, viewModel, child) {
-                return ListView.builder(
-                  padding: EdgeInsets.all(8.0),
-                  itemCount: items.length,
-                  itemBuilder: (context, index) {
-                    final item = items[index];
-                    return Card(
-                      child: ListTile(
-                        trailing: viewModel.isLoading
-                            ? Icon(Icons.hourglass_empty)
-                            : (viewModel.dataFetched
-                                ? Icon(Icons.check_circle)
-                                : Icon(Icons.cloud_download)),
-                        title: Text(item['title'] as String),
-                        subtitle: Text(item['subtitle'] as String),
-                        onTap: () {
-                          if (viewModel.dataFetched) {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => FlashCardScreen(),
-                              ),
-                            );
-                          } else {
-                            viewModel.downloadAndImportExcel();
-                          }
-                        },
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-        ],
+      body: Consumer<DataViewModel>(
+        builder: (context, viewModel, child) {
+          if (viewModel.isLoading) {
+            // isLoadingがtrueの場合、ダウンロード中メッセージを表示
+            return Center(
+              child: Text(
+                '単語帳データをダウンロードしています',
+                style: TextStyle(fontSize: 18),
+              ),
+            );
+          } else {
+            // isLoadingがfalseの場合、リストを表示
+            return Column(
+              children: [
+                Container(
+                    height: 241,
+                    width: 361,
+                    child: Image.asset('assets/images/home_humor1.png')),
+                const SizedBox(height: 32),
+                Expanded(
+                  child: ListView.builder(
+                    padding: EdgeInsets.all(8.0),
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      final item = items[index];
+                      return Card(
+                        child: ListTile(
+                          tileColor: item['color'] as Color,
+                          trailing: Icon(item['icon'] as IconData),
+                          title: Text(item['title'] as String),
+                          subtitle: Text(item['subtitle'] as String),
+                          onTap: () {
+                            if (viewModel.dataFetched) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => FlashCardScreen(),
+                                ),
+                              );
+                            } else {
+                              viewModel.downloadAndImportExcel();
+                            }
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
+          }
+        },
       ),
     );
   }
