08fa701ab5072f3cf5117ae1059be5cd252ea193
ffffgggg5 <ffffgggg5@icloud.com>
2024-10-23 17:41:24 +0900

    固定行をつくった

--- lib/pages/list_screen.dart
+++ lib/pages/list_screen.dart
@@ -33,19 +33,11 @@ class _ListScreenState extends State<ListScreen> {
       ),
       body: Consumer<DataViewModel>(
         builder: (context, viewModel, child) {
-          return viewModel.isLoading
-              ? const Center(
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      CircularProgressIndicator(),
-                      SizedBox(height: 20),
-                      Text('Downloading...'),
-                    ],
-                  ),
-                )
-              : Column(
-                  children: [
+                // 検索バー
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: TextField(
@@ -62,6 +54,66 @@ class _ListScreenState extends State<ListScreen> {
                         },
                       ),
                     ),
+
+                // 見出し行
+                Container(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 12.0, horizontal: 8.0),
+                  color: Color.fromARGB(192, 60, 176, 180), // 柔らかい背景色
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          'ID',
+                          style: TextStyle(
+                              fontFamily: 'ZenMaruGothic',
+                              fontWeight: FontWeight.w700, // Bold
+                              fontSize: 18,
+                              color: Colors.white),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 4,
+                        child: Text(
+                          '見出し語',
+                          style: TextStyle(
+                              fontFamily: 'ZenMaruGothic',
+                              fontWeight: FontWeight.w700, // Bold
+                              fontSize: 18,
+                              color: Colors.white),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Text(
+                          '次の復習日',
+                          style: TextStyle(
+                              fontFamily: 'ZenMaruGothic',
+                              fontWeight: FontWeight.w700, // Bold
+                              fontSize: 18,
+                              color: Colors.white),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Text(
+                          '状態',
+                          style: TextStyle(
+                              fontFamily: 'ZenMaruGothic',
+                              fontWeight: FontWeight.w700, // Bold
+                              fontSize: 18,
+                              color: Colors.white),
+                          textAlign: TextAlign.right,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(height: 1, thickness: 1),
+
+                // リスト表示
                     Expanded(
                       child: ListView.builder(
                         itemCount: viewModel.searchResults.length,
